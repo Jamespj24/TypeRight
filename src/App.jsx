8c5f7ac2e747fb45ce1467515d2ce08c4d7a949e
James <jamespj.dev@gmail.com>
2025-07-09 20:47:58 +0530

    Day 1 setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center text-blue-500 mt-10">
+      Hello Tailwind!
+    </div>
+  );
+
+}
+
+export default App
