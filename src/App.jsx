4bb958ee91255abc6ae750b51d98525be3411fb3
James <jamespj.dev@gmail.com>
2025-07-10 22:42:26 +0530

    Day 2 Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-center text-blue-500 mt-10">
-      Hello Tailwind!
+    <div>
+      <Header />
     </div>
-  );
 
+  )
 }
+
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+  return (
+
+    <h1 className="text-3xl font-bold text-center text-purple-500 mt-10"> TypeRight</h1>
+  );
+
+}
+export default Header;
